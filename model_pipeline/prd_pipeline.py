31797793db760d0c8690badcf8508d841d4681de
hbrown <hannah.brown@metoffice.gov.uk>
2022-06-21 14:57:52 +0100

    Code review updates, including adding doc strings, clearer varible naming and removing hard coded random state

--- model_pipeline/prd_pipeline.py
+++ model_pipeline/prd_pipeline.py
@@ -27,8 +27,10 @@ import pickle
 
 def build_model(nprof_features, nheights, nsinglvl_features):
     """
-    TODO: add documentation on the building of the model
-
+    This 1D convoluational neural network take a regression approach to predict 
+    precipitation on a column-wise basis. This model takes vertical profile features 
+    as it's input with the option to include single height level features if required. 
+    From these features it predicts deterministic precipitation values. 
     """
     #  TODO: should some of these parameters be specified somewhere centrally?
     profile_input = Input(shape=(nheights, nprof_features), name='profile_input')
@@ -65,8 +67,12 @@ def build_model(nprof_features, nheights, nsinglvl_features):
         
     return model
 
+
 def train_model(model, data_splits, hyperparameter_dict):
-    # TODO: these hyperparameters should be read in from somewhere?
+    """
+    This function trains the input model with the given data samples in data_splits. 
+    Hyperparameters use when fitting the model are defined in hyperparameter_dict.
+    """
     optimizer = tf.keras.optimizers.Adam(learning_rate=hyperparameter_dict['learning_rate'])
     model.compile(loss='mean_absolute_error', optimizer=optimizer)
 
@@ -79,27 +85,44 @@ def train_model(model, data_splits, hyperparameter_dict):
     
 
 def load_data(current_ws, dataset_name):
+    """
+    This function loads data from AzureML storage and returns it as a pandas dataframe 
+    """
     dataset = azureml.core.Dataset.get_by_name(current_ws, name=dataset_name)
     input_data = dataset.to_pandas_dataframe()    
     return input_data
 
 
 def sample_data(features_df, target_df, test_fraction=0.2, savefn=None, random_state=None):
+    """
+    This function creates data samples for training and testing machine learning
+    This function take two pandas dataframes as inputs:
+      - features_df contains data from feature columns
+      - target_df contains data from target columns
+    If a filename is provided for the savefn argument the test dataset is saved
+    to this file and only the training input and target data is returned. 
+    If savefn is None (default) then both the train and test input and target data 
+    samples are returned.
+    """
     n_samples = features_df.shape[0]
-    test_features = features_df.sample(
+    test_input = features_df.sample(
         int(n_samples*test_fraction), random_state=random_state)
-    train_features = features_df[~np.isin(features_df.index, test_features.index)]
-    test_target = target_df[np.isin(target_df.index, test_features.index)]
-    train_target = target_df[~np.isin(target_df.index, test_features.index)]
+    train_input = features_df[~np.isin(features_df.index, test_input.index)]
+    test_target = target_df[np.isin(target_df.index, test_input.index)]
+    train_target = target_df[~np.isin(target_df.index, test_input.index)]
     if savefn:
-        test_dataset = pd.concat([test_features, test_target], axis=1, sort=False)
+        test_dataset = pd.concat([test_input, test_target], axis=1, sort=False)
         test_dataset.to_csv(savefn)
-        return train_features, train_target
+        return train_input, train_target
     else: 
-        return train_features, train_target, test_features, test_target
+        return train_input, train_target, test_input, test_target
 
 
 def reshape_profile_features(df, features, data_dims_dict):
+    """
+    This function reshapes vertical profile data from tabular data 
+    back into data with a height dimension
+    """
     prof_feature_columns = [
         s for s in df.columns for vars in features if s.startswith(vars)]
     df = df[prof_feature_columns]
@@ -114,8 +137,10 @@ def reshape_profile_features(df, features, data_dims_dict):
 
 
 def load_test_data(test_fn, feature_dict, data_dims_dict):
-    """TO DO: where is the best place to store test data and scaler and 
-    therefore where do we want to point to?"""
+    """
+    This function can be used to load in test data and returns 
+    input and target data ready to be used to test an ML model.
+    """
     test_data = pd.read_csv(test_fn)
     with open('standardScaler.pkl', 'rb') as fin:
         standardScaler = pickle.load(fin)
@@ -138,6 +163,10 @@ def load_test_data(test_fn, feature_dict, data_dims_dict):
 
 
 def preprocess_data(input_data, feature_dict, test_fraction=0.2, test_savefn=None):
+    """
+    Preprocesses data ahead of ML modelling
+    """
+
     # drop NaN values in the dataset
     data = input_data.dropna()
 
@@ -154,21 +183,23 @@ def preprocess_data(input_data, feature_dict, test_fraction=0.2, test_savefn=Non
         'nsinglvl_features' :len(feature_dict['single_level']),
     }
     
-    features = data[prof_feature_columns + feature_dict['single_level']]
-    target = data[[feature_dict['target']]]
+    input_data = data[prof_feature_columns + feature_dict['single_level']]
+    target_data = data[[feature_dict['target']]]
     
+    random_state = np.random.RandomState()  # TO DO: how to log this in experiments!
+
     # Extract and save test dataset
-    reduced_features, reduced_target = sample_data(
-        features, target,
+    reduced_input_data, reduced_target_data = sample_data(
+        input_data, target_data,
         test_fraction=test_fraction,
         savefn=test_savefn,
-        random_state=0)
+        random_state=random_state)
 
     # Extract and return train and validate datasets
     X_train, y_train, X_val, y_val = sample_data(
-        reduced_features, reduced_target,
+        reduced_input_data, reduced_target_data,
         test_fraction=test_fraction/(1-test_fraction),
-        random_state=0)
+        random_state=random_state)
     
     # Scale data to have zero mean and standard deviation of one
     standardScaler = StandardScaler()
