87a753c43c3a12800c43044d2a66c7409c472eb7
DPeterK <peter.killick@informaticslab.co.uk>
2022-06-22 15:36:17 +0100

    Fix config

--- data_prep/mass_extract_workflow.py
+++ data_prep/mass_extract_workflow.py
@@ -14,9 +14,9 @@ from dagster import (
 import pandas as pd
 
 
-@op(required_resource_keys={"values"})
+@op(required_resource_keys={"setup"})
 def run_cmd(context, cmd):
-    sep = context.resources.values["sep"]
+    sep = context.resources.setup["sep"]
     get_dagster_logger().info(f"Running shell command: {cmd!r}")
     try:
         response = subprocess.check_output(cmd, shell=True)
@@ -58,21 +58,21 @@ def dates_to_extract(context):
     return dates
 
 
-@op(required_resource_keys={"values"}, out=DynamicOut())
+@op(required_resource_keys={"setup"}, out=DynamicOut())
 def get_mass_paths(context, dates):
-    mass_root = context.resources.values["mass_root"]
+    mass_root = context.resources.setup["mass_root"]
     mass_filename_template = context.op_config["mass_filename_template"]
     for i, dt in enumerate(dates):
         mass_path = os.path.join(mass_root, mass_filename_template.format(dt=dt))
         yield DynamicOutput(mass_path, mapping_key=f"path_{i}")
 
 
-@op(required_resource_keys={"values"})
+@op(required_resource_keys={"setup"})
 def retrieve_from_mass(context, mass_fname, temp_dir) -> str:
     """Get a file from MASS."""
-    mass_root = context.resources.values["mass_root"]
+    mass_root = context.resources.setup["mass_root"]
     mass_get_cmd_template = context.op_config["mass_get_cmd"]
-    dest_path = os.path.join(temp_dir.name, context.resources.values["dest_path"])
+    dest_path = os.path.join(temp_dir.name, context.resources.setup["dest_path"])
     mass_path = os.path.join(mass_root, mass_fname)
     mass_get_cmd = mass_get_cmd_template.format(
         args=context.op_config["mass_get_args"],
@@ -82,11 +82,11 @@ def retrieve_from_mass(context, mass_fname, temp_dir) -> str:
     return mass_get_cmd
 
 
-@op(required_resource_keys={"values"})
+@op(required_resource_keys={"setup"})
 def extract_mass_retrieval(context, temp_dir, mass_fname, _) -> str:
     """Extract a tar archive file retrieved from MASS."""
     untar_cmd_template = context.op_config["untar_cmd"]
-    dest_path = context.resources.values["dest_path"]
+    dest_path = context.resources.setup["dest_path"]
     extract_path = os.path.join(temp_dir.name, dest_path)
     mass_tar_name = os.path.basename(mass_fname)
     extracted_tar_name = os.path.join(extract_path, mass_tar_name)
@@ -97,7 +97,7 @@ def extract_mass_retrieval(context, temp_dir, mass_fname, _) -> str:
     return untar_cmd
 
 
-@op(required_resource_keys={"values"}, out=DynamicOut())
+@op(required_resource_keys={"setup"}, out=DynamicOut())
 def filter_mass_retrieval(context, temp_dir, _):
     """Filter the extracted archive from MASS for specific files of interest."""
     filter_products = context.op_config["products"]
@@ -113,7 +113,7 @@ def filter_mass_retrieval(context, temp_dir, _):
         gunzip_files.extend(glob.glob(os.path.join(temp_dir.name, variable_fname)))
 
     unzip_cmd_template = context.op_config["unzip_cmd"]
-    dest_root = context.resources.values["retrieve_path_root"]
+    dest_root = context.resources.setup["retrieve_path_root"]
     for i, zip_file in enumerate(gunzip_files):
         # XXX this won't work if our files have a file extension..?
         filename = os.path.splitext(os.path.basename(zip_file))[0]
@@ -125,28 +125,13 @@ def filter_mass_retrieval(context, temp_dir, _):
         yield DynamicOutput(unzip_cmd, mapping_key=f"unzip_{i}")
 
 
-# @op(
-#     required_resource_keys={"values"},
-#     out={"mass_root": Out(), "retrieve_path_root": Out()}
-# )
-# def get_resource_values(context):
-#     mass_root = context.resources.values["mass_root"]
-#     retrieve_path_root = context.resources.values["retrieve_path_root"]
-#     return mass_root, retrieve_path_root
-
-
-# @graph
-# def mass_retrieve_and_extract(mass_path):
-#     mass_tar_name = os.path.basename(mass_path)
-#     retrieve_from_mass(mass_path)
-#     extract_mass_retrieval(mass_tar_name)
-#     filter_mass_retrieval(mass_tar_name)
-
-
-@op(required_resource_keys={"values"})
+@op(required_resource_keys={"setup"})
 def logit(context, responses):
     """Record important output from cmd executions to a log file."""
-    sep = context.resources.values["sep"]
+    sep = context.resources.setup["sep"]
+    logdir = context.resources.setup["logdir"]
+    logfile = context.resources.setup["logfile"]
+    logfilename = os.path.join(logdir, logfile)
     if isinstance(responses, str):
         responses = [responses]
     for response in responses:
@@ -156,9 +141,9 @@ def logit(context, responses):
     return "done!"
 
 
-@op(required_resource_keys={"values"})
+@op(required_resource_keys={"setup"})
 def create_temp_dir(context):
-    retrieve_path_root = context.resources.values["retrieve_path_root"]
+    retrieve_path_root = context.resources.setup["retrieve_path_root"]
     tempdir = TemporaryDirectory(prefix=retrieve_path_root)
     get_dagster_logger().info(f"Temporary extract directory: {tempdir.name}")
     return tempdir
@@ -182,82 +167,22 @@ def mass_retrieve_and_extract(mass_fname):
     remove_temp_dir(tempdir, done)
 
 
-# @graph
-# def mass_retrieve_and_extract_v2(mass_fname):
-#     mass_root, retrieve_path_root = get_resource_values()
-#     mass_tar_name = os.path.basename(mass_fname)
-#     mass_path = os.path.join(mass_root, mass_fname)
-#     with TemporaryDirectory(prefix=retrieve_path_root) as tempdir:
-#         get_dagster_logger().info(f"Temporary extract directory: {tempdir}")
-#         run_cmd(retrieve_from_mass(mass_root, mass_fname, tempdir))
-#         run_cmd(extract_mass_retrieval(tempdir, mass_fname))
-#         unzip_cmds = filter_mass_retrieval(tempdir, retrieve_path_root)
-#         unzip_cmds.map(run_cmd).collect()
-
-
-@job(resource_defs={"values": make_values_resource(dest_path=str)})
+@job(
+    resource_defs={"setup": make_values_resource(
+        mass_root=str,
+        retrieve_path_root=str,
+        dest_path=str,
+        sep=str,
+        logdir=str,
+        logfile=str,
+    )}
+)
 def mass_extract():
     dates = dates_to_extract()
     paths = get_mass_paths(dates)
     paths.map(mass_retrieve_and_extract)
     # results.collect()
 
-
-radar_config = {
-    "resources": {
-        "values": {
-            "config": {
-                "mass_root": "moose:/adhoc/projects/radar_archive/data/comp/products/composites/",
-                "retrieve_path_root": "/scratch/dkillick/precip_rediagnosis/radar/",
-                "dest_path": "radar",
-            }
-        }
-    },
-    "ops": {
-        "dates_to_extract": {
-            "config": {
-                "datetime_str": "%Y-%m-%dT%H:%MZ",
-                "archive_time_chunk": "24",
-                "event_start": "2020-02-14T18:00Z",
-                "event_end": "2020-02-17T18:00Z",
-            }
-        },
-        "get_mass_paths": {
-            "config": {
-                "mass_filename_template": "{dt.year:04d}{dt.month:02d}{dt.day:02d}.tar",
-            }
-        },
-        "retrieve_from_mass": {
-            "config": {
-                "mass_get_cmd": "moo get {args} {src_paths} {dest_path}",
-                "mass_get_args": "-f",
-                # "dest_path": "radar",
-                # "dest_path": "./radar",  # for radar retrieval workflows
-            }
-        },
-        "extract_mass_retrieval": {
-            "config": {
-                "untar_cmd": 'tar -xf {path} --directory {dest_root}',
-                # "dest_path": "radar",
-            }
-        },
-        "filter_mass_data": {
-            "config": {
-                # "unzip_cmd": "gunzip {root}/{files}",
-                "unzip_cmd": "gunzip -c {zip_file} > {dest_path}",
-                "dest_path": "radar",
-                "products": [
-                    "nimrod_ng_radar_rainrate_composite",
-                    "nimrod_ng_radar_qualityproduct_composite",
-                ],
-                "variable_fname_timestamp": "*",
-                "variable_fname_template": "{timestamp}_{product}_{resolution}_{area}",
-                "variable_fname_resolution": "1km",
-                "variable_fname_area": "UK",
-            }
-        },
-    }
-}
 
 # job_result = mass_extract.execute_in_process(
 #     run_config=radar_config
