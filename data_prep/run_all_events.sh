6c2cbbd8ad462b4668bca4b7d7bc6d1f4e662f7a
stevehadd <stevehadd@yahoo.com>
2022-07-28 13:54:35 +0100

    #3 conveniencve script to run multiple event extractions.

---
+++ data_prep/run_all_events.sh
@@ -0,0 +1,6 @@
+./data_prep/submit_data_prep_spice.sh 2020_storm_ciara
+./data_prep/submit_data_prep_spice.sh 2020_storm_dennis
+./data_prep/submit_data_prep_spice.sh 2020_storm_ellen
+./data_prep/submit_data_prep_spice.sh 2020_storm_francis
+./data_prep/submit_data_prep_spice.sh 2021_storm_barra
+./data_prep/submit_data_prep_spice.sh 2022_storm_eunice_franklin
